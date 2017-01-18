f7df82fc7b691477831d327a10423e9ef6d29c5f
LastCryyy <lastcryyy@lastcryyys-mac.local>
2017-01-18 16:41:27 +0100

    Initial Commit

---
+++ puissance4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  puissance4
+//
+//  Created by LastCryyy on 18/01/2017.
+//  Copyright © 2017 LastCryyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
